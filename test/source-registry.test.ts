72ed795a1c271accbbfaec694bae9f9542c1f951
m-fujisawa7 <m.fujisawa@studio.design>
2026-08-27 15:58:09 +0900

    config: 埼玉県の行政ニーズ情報源を追加する

--- test/source-registry.test.ts
+++ test/source-registry.test.ts
@@ -312,15 +312,18 @@ describe('ignore_list_published_atの検証', () => {
       .map((source) => source.id)
       .sort();
     // 沖縄県の年度別発注情報は同じtrに日付があるが、募集期間の開始日・開札日・
-    // プロポーザル実施日であり掲載日ではない。相模原市と栃木県のHTML一覧は
-    // 有効な年付き掲載日を持たず、タイトル中の年度・対象日を誤ってpublishedAtに
-    // しないために使う。
+    // プロポーザル実施日であり掲載日ではない。埼玉県、相模原市、栃木県のHTML一覧は
+    // 有効な年付き掲載日を持たず、タイトル中の年度・対象日や共通の掲載日を誤って
+    // publishedAtにしないために使う。
     expect(configured).toEqual([
       'okinawa-consulting-procurement-2026',
       'okinawa-industry-digital-procurement-2026',
       'okinawa-information-procurement-2026',
       'sagamihara-procurement-news',
       'sagamihara-sounding',
+      'saitama-all-news',
+      'saitama-procurement-news',
+      'saitama-public-private-list',
       'tochigi-business-news',
       'tochigi-goods-rfi',
       'tochigi-outsourcing',
